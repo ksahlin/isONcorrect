beceb8c0a42fa559fc183a61d69e053896ddd81e
Kristoffer Sahlin <kxs624@psu.edu>
2019-01-26 21:07:31 -0500

    testing generation of new  augmented reference

--- modules/create_augmented_reference.py
+++ modules/create_augmented_reference.py
@@ -8,15 +8,57 @@ import tempfile
 from datetime import datetime 
 from sys import stdout
 
+import networkx as nx
+
+def longest_path(dot_graph_path):
+    G = nx.DiGraph()
+    for line in open(dot_graph_path, "r"):
+        if len(line.split("->")) == 2: # edge
+            n1, info = line.split("->")
+            n1 = n1.strip()
+            n2, info = info.strip().split("[")
+            n2 = n2.strip()
+            if "dotted" in info: # edge should not be created for substitutions 
+                continue  
+            elif "," not in info: # not in consensus path
+                weight = info.split("=")[1].strip().strip("]").strip('"')
+                # print("ok", weight)
+            else:
+                weight = info.split(",")[0].split("=")[1].strip().strip('"')
+                # print("bla", weight)
+            if int(weight) > 4:
+                G.add_edge(n1, n2, weight=int(weight))
+
+        elif "label" in line: # at node
+            node_id, info = line.split("[")
+            node_id = node_id.strip()
+            nucl = info.split(" - ")[1][0]
+            # print(node_id, nucl)
+            G.add_node(node_id, nucleotide=nucl)
 
 
 
-def run_spoa(reads, ref_out_file, spoa_out_file, spoa_path):
+
+
+
+    # G = nx.drawing.nx_pydot.read_dot(dot_graph_path)
+    # print(G.edges(data=True))
+    # print(G.nodes(data=True))
+    longest_path = nx.algorithms.dag.dag_longest_path(G)
+    # print(longest_path)
+    augmented_ref = "".join([ G.node[n]["nucleotide"] for n in longest_path])
+    # augmented_ref = "".join([ info["label"][-2] for n, info in G.nodes(data=True)])
+    print(augmented_ref)
+    print(len(augmented_ref))
+    # sys.exit()
+    return augmented_ref
+
+def run_spoa(reads, ref_out_file, spoa_out_file, spoa_path, dot_graph_path):
     with open(spoa_out_file, "w") as output_file:
         print('Running spoa...', end=' ')
         stdout.flush()
         null = open("/dev/null", "w")
-        subprocess.check_call([ spoa_path, reads, "-l", "0", "-r", "2", "--gap", "-2"], stdout=output_file, stderr=null)
+        subprocess.check_call([ spoa_path, reads, "-l", "0", "-r", "2", "--gap", "-2", "-d", dot_graph_path], stdout=output_file, stderr=null)
         print('Done.')
         stdout.flush()
     output_file.close()
@@ -24,6 +66,8 @@ def run_spoa(reads, ref_out_file, spoa_out_file, spoa_path):
     consensus = l[1].strip()
     msa = [s.strip() for s in l[3:]]
     print(consensus)
+    print(len(consensus))
+    # print(msa)
     
     r = open(ref_out_file, "w")
     r.write(">{0}\n{1}".format("reference", consensus))
