2aacd9f054d4ed162d8343e8fbec839cea85c69f
Kristoffer Sahlin <kxs624@psu.edu>
2019-02-22 18:01:32 -0500

    ctd working on error correction

--- modules/create_augmented_reference.py
+++ modules/create_augmented_reference.py
@@ -107,21 +107,43 @@ def run_spoa_affine(reads, ref_out_file, spoa_out_file, spoa_path, dot_graph_pat
         print('Running spoa...', end=' ')
         stdout.flush()
         null = open("/dev/null", "w")
-        subprocess.check_call([ spoa_path, "-q", reads, "-l", "2", "-r", "2", "-o","-4", "-e", "0", "-d", dot_graph_path], stderr=output_file, stdout=null)
+        subprocess.check_call([ spoa_path, "-q", reads, "-l", "2", "-r", "2", "-x", "-8", "-m", "10", "-o","-8", "-e", "-1", "-d", dot_graph_path], stderr=output_file, stdout=null)
         print('Done.')
         stdout.flush()
     output_file.close()
     l = open(spoa_out_file, "r").readlines()
     consensus = l[1].strip()
     msa = [s.strip() for s in l[3:]]
-    print("affine:", consensus)
-    print(len(consensus))
+    print("affine heaviest path:", consensus, len(consensus))
+    print()
     # print(msa)
     
     r = open(ref_out_file, "w")
     r.write(">{0}\n{1}".format("reference", consensus))
     r.close()
+    return consensus, msa
 
+def run_spoa_affine_v2_0_3(reads, ref_out_file, spoa_out_file, spoa_path, dot_graph_path):
+    with open(spoa_out_file, "w") as output_file:
+        print('Running spoa...', end=' ')
+        stdout.flush()
+        null = open("/dev/null", "w")
+        # print(spoa_path, "-l", "2", "-r", "2", "-g", "-4", "-e", "0", "-d", dot_graph_path, reads)
+        subprocess.check_call([ spoa_path, "-l", "2", "-r", "2", "-x", "-8", "-m", "10", "-g", "-8", "-e", "-1", "-d", dot_graph_path, reads], stdout=output_file, stderr=null)
+        # subprocess.check_call([ spoa_path, "-q", reads, "-l", "2", "-r", "2", "-o","-4", "-e", "0", "-d", dot_graph_path], stderr=output_file, stdout=null)
+        print('Done.')
+        stdout.flush()
+    output_file.close()
+    l = open(spoa_out_file, "r").readlines()
+    consensus = l[1].strip()
+    msa = [s.strip() for s in l[3:]]
+    print("affine_v2_0_2 heaviest path:", consensus, len(consensus))
+    print()
+    # print(msa)
+    
+    r = open(ref_out_file, "w")
+    r.write(">{0}\n{1}".format("reference", consensus))
+    r.close()
     return consensus, msa
 
 def run_spoa(reads, ref_out_file, spoa_out_file, spoa_path, dot_graph_path):
@@ -151,7 +173,7 @@ def run_spoa(reads, ref_out_file, spoa_out_file, spoa_path, dot_graph_path):
 
 # For eventual De Bruijn graph approach
 import itertools
-from collections import defaultdict
+from collections import defaultdict, deque
 def kmer_counter(reads):
     k_size = 13
     count = defaultdict(int)
@@ -163,7 +185,8 @@ def kmer_counter(reads):
             count[kmer] += 1
 
     cnt_sorted = sorted(count.items(), key = lambda x: x[1], reverse=True)
-    print(cnt_sorted[:300])
+    print(len(cnt_sorted),cnt_sorted)
+    return count
 
 
 if __name__ == '__main__':
