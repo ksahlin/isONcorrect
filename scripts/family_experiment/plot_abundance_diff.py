82b41bc86b72d8a8b5db3d864b00e45f82b1d29f
Kristoffer Sahlin <kxs624@psu.edu>
2019-06-28 17:40:38 -0400

    ctd

--- scripts/family_experiment/plot_abundance_diff.py
+++ scripts/family_experiment/plot_abundance_diff.py
@@ -23,10 +23,17 @@ data=sys.argv[1]
 indata = pd.read_csv(data, sep='\t')
 # y=sys.argv[3]
 
-g = sns.catplot(x="abundance_original", y="abundance_corrected", col="Depth", col_wrap=3,
-            data=indata, kind="strip", aspect=1)
+# g = sns.catplot(x="abundance_original", y="abundance_corrected", col="Depth", col_wrap=3,
+#             data=indata, kind="point", aspect=1)
+# ax = sns.scatterplot(x="abundance_original", y="abundance_corrected", #col="Depth", # col_wrap=3, #hue="time",
+#                       data=indata)
+g = sns.lmplot(x="abundance_original", y="abundance_corrected", col="Depth", # hue="day",
+                data=indata, col_wrap=2, height=3)
 
 # g.set(ylim=(0,100))
+# ax.set_ylabel("Abundance after correction")
+# ax.set_xlabel("Abundance before correction")
+
 g.set_ylabels("Abundance after correction")
 g.set_xlabels("Abundance before correction")
 
