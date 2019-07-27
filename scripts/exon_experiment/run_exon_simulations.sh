a5122e27680938cf44e43d667502f6c8c9c34dc1
Kristoffer Sahlin <kxs624@psu.edu>
2019-07-27 16:11:45 +0300

    continued making simulated experiments ready from cluster and snakemake.

--- scripts/exon_experiment/run_exon_simulations.sh
+++ scripts/exon_experiment/run_exon_simulations.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-outbase="/Users/kxs624/tmp/ISONCORRECT/SIMULATED_DATA/3_bp_exons_isoncorrect3/exon_experiment"
-experiment_dir="/Users/kxs624/Documents/workspace/isONcorrect/scripts/exon_experiment"
+
+# RUN scripts e.g. as: ./run_exon_simulations.sh /Users/kxs624/Documents/workspace/isONcorrect/  /Users/kxs624/tmp/ISONCORRECT/SIMULATED_DATA/exon_experiment/
+
+inbase=$1/
+outroot=$2/
+
+
+outbase=$outroot/
+experiment_dir=$inbase"/scripts/exon_experiment"
+eval_dir=$inbase"/scripts/"
+
 mkdir -p $outbase
 
 # if [ "$1" == "exact" ]
 #     then
 #         echo "Using exact mode"
-#         results_file=$outbase/"results_exact.csv"
+#         summary_file=$outbase/"results_exact.csv"
 #         plot_file=$outbase/"results_exact"
 
 #     else
 #         echo "Using approximate mode"
-#         results_file=$outbase/"results_approximate.csv"        
+#         summary_file=$outbase/"results_approximate.csv"        
 #         plot_file=$outbase/"results_approximate"
 # fi
 
-results_file=$outbase/"summary.csv"
-all_results_file=$outbase/"results.csv"
+summary_file=$outbase/"summary.csv"
+results_file=$outbase/"results.csv"
 plot_file=$outbase/"summary"
 
 
-echo -n  "id","type","Depth","p","tot","err","subs","ins","del","Total","Substitutions","Insertions","Deletions","switches"$'\n' > $results_file
-echo -n  "id","type","Depth","p","q_acc","r_acc", "total_errors","identity","subs","ins","del","switch","abundance"$'\n' > $all_results_file
+echo -n  "id","type","Depth","p","tot","err","subs","ins","del","Total","Substitutions","Insertions","Deletions","switches"$'\n' > $summary_file
+echo -n  "id","type","Depth","p","q_acc","r_acc", "total_errors","error_rate","subs","ins","del","switch","abundance"$'\n' > $results_file
 
 
-for id in $(seq 1 1 1)  
+for id in $(seq 1 1 3)  
 do 
-    for depth in 20 #10 20 50 # 100 200 500
+    for depth in 20 50 #10 20 50 # 100 200 500
     do
         # which python
         python $experiment_dir/simulate_reads.py --sim_genome_len 300 --coords 0 100 200 300 --outfolder $outbase/$depth/$id/ --probs 1.0 $p 1.0  --nr_reads $depth > /dev/null
@@ -41,35 +50,34 @@ do
             #         python /users/kxs624/Documents/workspace/isONcorrect/isONcorrect3 --fastq $outbase/$depth/$id/$p/reads.fq   --outfolder $outbase/$depth/$id/$p/isoncorrect/ --k 7 --w 10 --xmax 80   &> /dev/null
             # fi
 
-            python /users/kxs624/Documents/workspace/isONcorrect/isONcorrect3 --fastq $outbase/$depth/$id/$p/reads.fq   --outfolder $outbase/$depth/$id/$p/isoncorrect/ --k 7 --w 10 --xmax 80  &> /dev/null            
-            python $experiment_dir/../family_experiment/evaluate_simulated_reads.py  $outbase/$depth/$id/$p/isoncorrect/corrected_reads.fastq  $outbase/$depth/$id/isoforms.fa $outbase/$depth/$id/$p/isoforms_abundance.fa $outbase/$depth/$id/$p/isoncorrect/evaluation > /dev/null
+            python /users/kxs624/Documents/workspace/isONcorrect/isONcorrect3 --fastq $outbase/$depth/$id/$p/reads.fq   --outfolder $outbase/$depth/$id/$p/isoncorrect/ --k 7 --w 10 --xmax 80  > /dev/null            
+            python $eval_dir/evaluate_simulated_reads.py  $outbase/$depth/$id/$p/isoncorrect/corrected_reads.fastq  $outbase/$depth/$id/isoforms.fa $outbase/$depth/$id/$p/isoncorrect/evaluation > /dev/null
             echo -n  $id,approx,$depth,$p,&& head -n 1 $outbase/$depth/$id/$p/isoncorrect/evaluation/summary.csv 
-            echo -n  $id,approx,$depth,$p, >> $results_file && head -n 1 $outbase/$depth/$id/$p/isoncorrect/evaluation/summary.csv >> $results_file
-            awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$p  '{if (NR!=1) {print awk_id",approx,"awk_depth","awk_p","$0}}'  $outbase/$depth/$id/$p/isoncorrect/evaluation/results.csv >> $all_results_file
+            echo -n  $id,approx,$depth,$p, >> $summary_file && head -n 1 $outbase/$depth/$id/$p/isoncorrect/evaluation/summary.csv >> $summary_file
+            awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$p  '{if (NR!=1) {print awk_id",approx,"awk_depth","awk_p","$0}}'  $outbase/$depth/$id/$p/isoncorrect/evaluation/results.csv >> $results_file
 
-            python /users/kxs624/Documents/workspace/isONcorrect/isONcorrect3 --fastq $outbase/$depth/$id/$p/reads.fq   --outfolder $outbase/$depth/$id/$p/isoncorrect/ --k 7 --w 10 --xmax 80 --exact   &> /dev/null            
-            python $experiment_dir/../family_experiment/evaluate_simulated_reads.py  $outbase/$depth/$id/$p/isoncorrect/corrected_reads.fastq  $outbase/$depth/$id/isoforms.fa $outbase/$depth/$id/$p/isoforms_abundance.fa $outbase/$depth/$id/$p/isoncorrect_exact/evaluation > /dev/null
+            python /users/kxs624/Documents/workspace/isONcorrect/isONcorrect3 --fastq $outbase/$depth/$id/$p/reads.fq   --outfolder $outbase/$depth/$id/$p/isoncorrect/ --k 7 --w 10 --xmax 80 --exact   > /dev/null            
+            python $eval_dir/evaluate_simulated_reads.py  $outbase/$depth/$id/$p/isoncorrect/corrected_reads.fastq  $outbase/$depth/$id/isoforms.fa $outbase/$depth/$id/$p/isoncorrect_exact/evaluation > /dev/null
             echo -n  $id,exact,$depth,$p,&& head -n 1 $outbase/$depth/$id/$p/isoncorrect_exact/evaluation/summary.csv 
-            echo -n  $id,exact,$depth,$p, >> $results_file && head -n 1 $outbase/$depth/$id/$p/isoncorrect_exact/evaluation/summary.csv >> $results_file
-            awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$p '{if (NR!=1) {print awk_id",exact,"awk_depth","awk_p","$0}}'  $outbase/$depth/$id/$p/isoncorrect_exact/evaluation/results.csv >> $all_results_file
+            echo -n  $id,exact,$depth,$p, >> $summary_file && head -n 1 $outbase/$depth/$id/$p/isoncorrect_exact/evaluation/summary.csv >> $summary_file
+            awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$p '{if (NR!=1) {print awk_id",exact,"awk_depth","awk_p","$0}}'  $outbase/$depth/$id/$p/isoncorrect_exact/evaluation/results.csv >> $results_file
 
             fastq2fasta $outbase/$depth/$id/$p/reads.fq $outbase/$depth/$id/$p/reads.fa
-            python $experiment_dir/../family_experiment/evaluate_simulated_reads.py   $outbase/$depth/$id/$p/reads.fa $outbase/$depth/$id/isoforms.fa $outbase/$depth/$id/$p/isoforms_abundance.fa $outbase/$depth/$id/$p/evaluation_reads  > /dev/null
+            python $eval_dir/evaluate_simulated_reads.py   $outbase/$depth/$id/$p/reads.fa $outbase/$depth/$id/isoforms.fa $outbase/$depth/$id/$p/evaluation_reads  > /dev/null
             echo -n  $id,original,$depth,$p,&& head -n 1 $outbase/$depth/$id/$p/evaluation_reads/summary.csv 
-            echo -n  $id,original,$depth,$p, >> $results_file && head -n 1 $outbase/$depth/$id/$p/evaluation_reads/summary.csv  >> $results_file
-            awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$p '{if (NR!=1) {print awk_id",original,"awk_depth","awk_p","$0}}'  $outbase/$depth/$id/$p/evaluation_reads/results.csv >> $all_results_file
+            echo -n  $id,original,$depth,$p, >> $summary_file && head -n 1 $outbase/$depth/$id/$p/evaluation_reads/summary.csv  >> $summary_file
+            awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$p '{if (NR!=1) {print awk_id",original,"awk_depth","awk_p","$0}}'  $outbase/$depth/$id/$p/evaluation_reads/results.csv >> $results_file
 
         done
     done
 done
 
 
-echo  $experiment_dir/plot_exon_data.py $results_file $plot_file
-python $experiment_dir/plot_exon_data.py $results_file $plot_file"_tot.pdf" Total
-python $experiment_dir/plot_exon_data.py $results_file $plot_file"_subs.pdf" Substitutions
-python $experiment_dir/plot_exon_data.py $results_file $plot_file"_ind.pdf" Insertions
-python $experiment_dir/plot_exon_data.py $results_file $plot_file"_del.pdf" Deletions
+echo  $experiment_dir/plot_exon_data.py $summary_file $plot_file
+python $experiment_dir/plot_exon_data.py $results_file $plot_file"_tot.pdf" error_rate
+python $experiment_dir/plot_exon_data.py $summary_file $plot_file"_subs.pdf" Substitutions
+python $experiment_dir/plot_exon_data.py $summary_file $plot_file"_ind.pdf" Insertions
+python $experiment_dir/plot_exon_data.py $summary_file $plot_file"_del.pdf" Deletions
 
-python $experiment_dir/plot_frac_switches.py $all_results_file $plot_file"_overcorrected.pdf" 
+python $experiment_dir/plot_frac_switches.py $results_file $plot_file"_overcorrected.pdf" 
 
-# python $experiment_dir/../family_experiment/plot_abundance_diff.py $results_file $plot_file"_abundance_diff.pdf" 
--- scripts/family_experiment/plot_error_rates.py
+++ scripts/family_experiment/plot_error_rates.py
@@ -19,16 +19,24 @@ import pandas as pd
 sns.set(style="whitegrid")
 
 data=sys.argv[1]
-# f = open(data, "r")
-indata = pd.read_csv(data)
+df = pd.read_csv(data)
+print(len(df))
+df['q_acc'] = df['q_acc'].apply(lambda x: x.split("_")[0])
+indata = df.loc[df['q_acc'] == df['r_acc']]
+print(len(indata))
+
 y=sys.argv[3]
-g = sns.catplot(x="mut", y=y, col="Depth", col_wrap=3,
+g = sns.catplot(x="Depth", y=y, #col="Depth", col_wrap=3,
             data=indata, hue="type", hue_order= ["exact", "approx", "original"],
             kind="violin", aspect=1)
 
-g.set(ylim=(0,15))
-g.set_ylabels("Error rate %")
-g.set_xlabels("Mutation rate")
+
+g.ax.set_yscale("log")
+g.set(ylim=(0.01,15))
+g.ax.set_yticks([1e-1,1e0,1e1,1e2])
+g.set_yticklabels(("0.1%","1%","10%","100%"), visible = True)
+g.set_ylabels("Error rate")
+g.set_xlabels("Read depth")
 
 # ax = sns.boxplot(x="p", y=y, hue = "type", data=indata)
 # ax.set_ylim(0,15)
