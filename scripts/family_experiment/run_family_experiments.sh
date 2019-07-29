c82b764deead1801a8869a459ee079017391e003
Kristoffer Sahlin <kxs624@psu.edu>
2019-07-29 20:22:24 +0300

    tested some techniques

--- scripts/family_experiment/run_family_experiments.sh
+++ scripts/family_experiment/run_family_experiments.sh
@@ -38,87 +38,87 @@ summary_file=$outbase/"summary_"$mut_rate"_"$abundance"_"$gene_member".csv"
 results_file=$outbase/"results_"$mut_rate"_"$abundance"_"$gene_member".csv"
 plot_file=$outbase/"summary_"$mut_rate"_"$abundance"_"$gene_member
 
-# echo -n  "id","type","Depth","mut","tot","err","subs","ins","del","Total","Substitutions","Insertions","Deletions"$'\n' > $summary_file
-# echo -n  "id","type","Depth","mut","q_acc","r_acc","total_errors","error_rate","subs","ins","del","switch","abundance"$'\n' > $results_file
+echo -n  "id","type","Depth","mut","tot","err","subs","ins","del","Total","Substitutions","Insertions","Deletions"$'\n' > $summary_file
+echo -n  "id","type","Depth","mut","q_acc","r_acc","total_errors","error_rate","subs","ins","del","switch","abundance"$'\n' > $results_file
 
 
-# python $experiment_dir/get_exons.py $database $outbase/"exons" > /dev/null
+python $experiment_dir/get_exons.py $database $outbase/"exons" > /dev/null
 
-# for id in $(seq 1 1 3)  
-# do 
-#     python $experiment_dir/generate_transcripts.py --exon_file $outbase/exons/$gene_member"_exons.fa"  $outbase/$id/biological_material.fa --gene_member $gene_member  --family_size $family_size --isoform_distribution exponential  --mutation_rate $mut_rate  > /dev/null
-#     python $experiment_dir/generate_abundance.py --transcript_file $outbase/$id/biological_material.fa --abundance $abundance $outbase/$id/biological_material_abundance.fa   > /dev/null
+for id in $(seq 1 1 3)  
+do 
+    python $experiment_dir/generate_transcripts.py --exon_file $outbase/exons/$gene_member"_exons.fa"  $outbase/$id/biological_material.fa --gene_member $gene_member  --family_size $family_size --isoform_distribution exponential  --mutation_rate $mut_rate  > /dev/null
 
-#     for depth in 10 20 50 #100 #200 #20 #20 50 100 #10 20 #50 # 100 200 500 1000 5000 10000
-#     do
-#         python $experiment_dir/generate_ont_reads.py $outbase/$id/biological_material_abundance.fa $outbase/$depth/$id/reads.fq $depth > /dev/null
+    for depth in 10 20 #50 #100 #200 #20 #20 50 100 #10 20 #50 # 100 200 500 1000 5000 10000
+    do
+        python $experiment_dir/generate_abundance.py --transcript_file $outbase/$id/biological_material.fa --abundance $abundance $outbase/$id/$depth/biological_material_abundance.fa   > /dev/null
+        python $experiment_dir/generate_ont_reads.py $outbase/$id/$depth/biological_material_abundance.fa $outbase/$id/$depth/reads.fq $depth > /dev/null
 
-#         python $inbase/isONcorrect3 --fastq $outbase/$depth/$id/reads.fq   --outfolder $outbase/$depth/$id/isoncorrect/ --k 7 --w 10 --xmax 80  > /dev/null            
-#         python $eval_dir/evaluate_simulated_reads.py  $outbase/$depth/$id/isoncorrect/corrected_reads.fastq  $outbase/$id/biological_material.fa $outbase/$depth/$id/isoncorrect/evaluation > /dev/null
-#         echo -n  $id,approx,$depth,$mut_rate,&& head -n 1 $outbase/$depth/$id/isoncorrect/evaluation/summary.csv 
-#         echo -n  $id,approx,$depth,$mut_rate, >> $summary_file && head -n 1 $outbase/$depth/$id/isoncorrect/evaluation/summary.csv >> $summary_file
-#         awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$mut_rate  '{if (NR!=1) {print awk_id",approx,"awk_depth","awk_p","$0}}'  $outbase/$depth/$id/isoncorrect/evaluation/results.csv >> $results_file
+        python $inbase/isONcorrect3 --fastq $outbase/$id/$depth/reads.fq   --outfolder $outbase/$id/$depth/isoncorrect/ --T 0.1 --k 7 --w 10 --xmax 80  &> /dev/null            
+        python $eval_dir/evaluate_simulated_reads.py  $outbase/$id/$depth/isoncorrect/corrected_reads.fastq  $outbase/$id/biological_material.fa $outbase/$id/$depth/isoncorrect/evaluation > /dev/null
+        echo -n  $id,approx,$depth,$mut_rate,&& head -n 1 $outbase/$id/$depth/isoncorrect/evaluation/summary.csv 
+        echo -n  $id,approx,$depth,$mut_rate, >> $summary_file && head -n 1 $outbase/$id/$depth/isoncorrect/evaluation/summary.csv >> $summary_file
+        awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$mut_rate  '{if (NR!=1) {print awk_id",approx,"awk_depth","awk_p","$0}}'  $outbase/$id/$depth/isoncorrect/evaluation/results.csv >> $results_file
 
         
-#         # for line in $(cat $outbase/$depth/$id/isoncorrect/evaluation/results2.csv ); do
-#         #         # echo "tester: $line"
-#         #         echo -n  $id$'\t'$depth$'\t'$mut_rate$'\t' >> $results_file && echo $line >> $results_file
-#         # done
-#         # # cat $outbase/$depth/$id/isoncorrect/evaluation/results2.csv >> $results_file
+        # for line in $(cat $outbase/$id/$depth/isoncorrect/evaluation/results2.csv ); do
+        #         # echo "tester: $line"
+        #         echo -n  $id$'\t'$depth$'\t'$mut_rate$'\t' >> $results_file && echo $line >> $results_file
+        # done
+        # # cat $outbase/$id/$depth/isoncorrect/evaluation/results2.csv >> $results_file
 
 
-#         fastq2fasta $outbase/$depth/$id/reads.fq $outbase/$depth/$id/reads.fa
-#         python $eval_dir/evaluate_simulated_reads.py   $outbase/$depth/$id/reads.fa $outbase/$id/biological_material.fa $outbase/$depth/$id/evaluation_reads > /dev/null
-#         echo -n  $id,original,$depth,$mut_rate,&& head -n 1 $outbase/$depth/$id/evaluation_reads/summary.csv 
-#         echo -n  $id,original,$depth,$mut_rate, >> $summary_file && head -n 1 $outbase/$depth/$id/evaluation_reads/summary.csv  >> $summary_file
-#         awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$mut_rate '{if (NR!=1) {print awk_id",original,"awk_depth","awk_p","$0}}'  $outbase/$depth/$id/evaluation_reads/results.csv >> $results_file
+        fastq2fasta $outbase/$id/$depth/reads.fq $outbase/$id/$depth/reads.fa
+        python $eval_dir/evaluate_simulated_reads.py   $outbase/$id/$depth/reads.fa $outbase/$id/biological_material.fa $outbase/$id/$depth/evaluation_reads > /dev/null
+        echo -n  $id,original,$depth,$mut_rate,&& head -n 1 $outbase/$id/$depth/evaluation_reads/summary.csv 
+        echo -n  $id,original,$depth,$mut_rate, >> $summary_file && head -n 1 $outbase/$id/$depth/evaluation_reads/summary.csv  >> $summary_file
+        awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$mut_rate '{if (NR!=1) {print awk_id",original,"awk_depth","awk_p","$0}}'  $outbase/$id/$depth/evaluation_reads/results.csv >> $results_file
 
 
-#         if [ "$depth" -gt 101 ];
-#         then 
-#             echo "Depth greater than 100, skipping exact";
-#             continue
-#         else
-#             python $inbase/isONcorrect3 --fastq $outbase/$depth/$id/reads.fq   --outfolder $outbase/$depth/$id/isoncorrect_exact/ --k 7 --w 10 --xmax 80 --exact   > /dev/null            
-#             python $eval_dir/evaluate_simulated_reads.py  $outbase/$depth/$id/isoncorrect_exact/corrected_reads.fastq  $outbase/$id/biological_material.fa $outbase/$depth/$id/isoncorrect_exact/evaluation > /dev/null
-#             echo -n  $id,exact,$depth,$mut_rate,&& head -n 1 $outbase/$depth/$id/isoncorrect_exact/evaluation/summary.csv 
-#             echo -n  $id,exact,$depth,$mut_rate, >> $summary_file && head -n 1 $outbase/$depth/$id/isoncorrect_exact/evaluation/summary.csv >> $summary_file
-#             awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$mut_rate '{if (NR!=1) {print awk_id",exact,"awk_depth","awk_p","$0}}'  $outbase/$depth/$id/isoncorrect_exact/evaluation/results.csv >> $results_file
+        if [ "$depth" -gt 101 ];
+        then 
+            echo "Depth greater than 100, skipping exact";
+            continue
+        else
+            python $inbase/isONcorrect3 --fastq $outbase/$id/$depth/reads.fq   --outfolder $outbase/$id/$depth/isoncorrect_exact/ --T 0.1 --k 7 --w 10 --xmax 80 --exact   &> /dev/null            
+            python $eval_dir/evaluate_simulated_reads.py  $outbase/$id/$depth/isoncorrect_exact/corrected_reads.fastq  $outbase/$id/biological_material.fa $outbase/$id/$depth/isoncorrect_exact/evaluation > /dev/null
+            echo -n  $id,exact,$depth,$mut_rate,&& head -n 1 $outbase/$id/$depth/isoncorrect_exact/evaluation/summary.csv 
+            echo -n  $id,exact,$depth,$mut_rate, >> $summary_file && head -n 1 $outbase/$id/$depth/isoncorrect_exact/evaluation/summary.csv >> $summary_file
+            awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$mut_rate '{if (NR!=1) {print awk_id",exact,"awk_depth","awk_p","$0}}'  $outbase/$id/$depth/isoncorrect_exact/evaluation/results.csv >> $results_file
 
-#         fi;
-
-
-#         # ########################################
-#         # # # Evaluate error rates TMP TEST
-#         # corrected_reads_mappings=$outbase/"corrected_"$depth".sam"
-#         # original_reads_mappings=$outbase/"original_"$depth".sam"
-#         # error_rates_file=$outbase/"error_rates_"$depth".csv"
-#         # tmp_experiment_dir="$inbase/scripts/full_transcriptome_exp"
-#         # corrected_reads_fastq=$outbase/$depth/$id/"isoncorrect"/"corrected_reads.fastq"
-#         # database=$outbase/$id/"biological_material.fa"
-#         # error_plot=$outbase/$depth/$id/"error_rates_"$depth".pdf" 
-#         # python $tmp_experiment_dir/get_error_rates.py  $database $outbase/$depth/$id/reads.fq  $corrected_reads_fastq  > $error_rates_file #$outbase/$depth/$id/isoncorrect/evaluation #&> /dev/null
-#         # python $tmp_experiment_dir/plot_error_rates.py $error_rates_file  $error_plot
-#         # # ######################
-
-#         # # ## Map reads
-#         # minimap2 -ax splice --eqx $database -uf -k14 $corrected_reads_fastq > $corrected_reads_mappings
-#         # minimap2 -ax splice --eqx $database  -uf -k14 $outbase/$depth/$id/reads.fq > $original_reads_mappings
-#         # # ###################
-
-#         # # # Evaluate chnage in expression levels compared to true expression
-#         # abundance_file=$outbase/$depth/$id/"abundance.csv"
-#         # > $abundance_file
-#         # echo -n  "id","cov_aln","cov_true","seq","type"$'\n' > $abundance_file
-#         # python $tmp_experiment_dir/get_abundance.py  $corrected_reads_mappings "corrected" >> $abundance_file
-#         # python $tmp_experiment_dir/get_abundance.py $original_reads_mappings  "original" >>  $abundance_file
-#         # abundance_plot=$outbase/$depth/$id/"abundance.pdf" 
-#         # python $tmp_experiment_dir/plot_abundance.py $abundance_file "transcript" $abundance_plot
-#         # ######################################################################
+        fi;
 
 
-#     done
-# done
+        # ########################################
+        # # # Evaluate error rates TMP TEST
+        # corrected_reads_mappings=$outbase/"corrected_"$depth".sam"
+        # original_reads_mappings=$outbase/"original_"$depth".sam"
+        # error_rates_file=$outbase/"error_rates_"$depth".csv"
+        # tmp_experiment_dir="$inbase/scripts/full_transcriptome_exp"
+        # corrected_reads_fastq=$outbase/$id/$depth/"isoncorrect"/"corrected_reads.fastq"
+        # database=$outbase/$id/"biological_material.fa"
+        # error_plot=$outbase/$id/$depth/"error_rates_"$depth".pdf" 
+        # python $tmp_experiment_dir/get_error_rates.py  $database $outbase/$id/$depth/reads.fq  $corrected_reads_fastq  > $error_rates_file #$outbase/$id/$depth/isoncorrect/evaluation #&> /dev/null
+        # python $tmp_experiment_dir/plot_error_rates.py $error_rates_file  $error_plot
+        # # ######################
+
+        # # ## Map reads
+        # minimap2 -ax splice --eqx $database -uf -k14 $corrected_reads_fastq > $corrected_reads_mappings
+        # minimap2 -ax splice --eqx $database  -uf -k14 $outbase/$id/$depth/reads.fq > $original_reads_mappings
+        # # ###################
+
+        # # # Evaluate chnage in expression levels compared to true expression
+        # abundance_file=$outbase/$id/$depth/"abundance.csv"
+        # > $abundance_file
+        # echo -n  "id","cov_aln","cov_true","seq","type"$'\n' > $abundance_file
+        # python $tmp_experiment_dir/get_abundance.py  $corrected_reads_mappings "corrected" >> $abundance_file
+        # python $tmp_experiment_dir/get_abundance.py $original_reads_mappings  "original" >>  $abundance_file
+        # abundance_plot=$outbase/$id/$depth/"abundance.pdf" 
+        # python $tmp_experiment_dir/plot_abundance.py $abundance_file "transcript" $abundance_plot
+        # ######################################################################
+
+
+    done
+done
 
 
 # echo  $experiment_dir/plot_error_rates.py $summary_file $plot_file
