95aa6f74e6126173fa1a53f269f506ab6a3b2689
Kristoffer Sahlin <kxs624@psu.edu>
2019-07-30 14:40:36 +0300

    fixes

--- scripts/full_transcriptome_exp/run_full_experiment.sh
+++ scripts/full_transcriptome_exp/run_full_experiment.sh
@@ -39,45 +39,45 @@ original_reads_mappings=$outbase/"original_"$depth".sam"
 results_file=$outbase/"results_"$depth".csv"
 echo -n  "id","type","Depth","mut","q_acc","r_acc","total_errors","error_rate","subs","ins","del","switch","abundance"$'\n' > $results_file
 
-# Remove redundant
+# # Remove redundant
 database_filtered=$outbase/transcriptsfiltered.fa
-python $experiment_dir/remove_redundant.py $database $database_filtered
+# python $experiment_dir/remove_redundant.py $database $database_filtered
 
-# Simulate reads
-echo  python $experiment_dir/simulate_reads.py $database_filtered $outbase/$id/$depth/reads.fq $depth
-python $experiment_dir/simulate_reads.py $database_filtered $outbase/$id/$depth/reads.fq $depth #&> /dev/null
-###############
+# # Simulate reads
+# echo  python $experiment_dir/simulate_reads.py $database_filtered $outbase/$depth/reads.fq $depth
+# python $experiment_dir/simulate_reads.py $database_filtered $outbase/$depth/reads.fq $depth #&> /dev/null
+# ###############
 
 # #  Remove longest transcripts
-# python $experiment_dir/filter_fastq.py $outbase/$id/$depth/reads.fq 20000
-# mv $outbase/$id/$depth/reads.fq_filtered.fq $outbase/$id/$depth/reads.fq
+# python $experiment_dir/filter_fastq.py $outbase/$depth/reads.fq 20000
+# mv $outbase/$depth/reads.fq_filtered.fq $outbase/$depth/reads.fq
 # ############################
 
 # Cluster with isONclust      
-rm -rf $outbase/$id/$depth/isonclust/sorted.fastq
-echo python $isonclust_dir/isONclust --fastq $outbase/$id/$depth/reads.fq --outfolder $outbase/$id/$depth/isonclust/ --k 13 --w 25 --q 6.0 --t $cores
-python $isonclust_dir/isONclust --fastq $outbase/$id/$depth/reads.fq --outfolder $outbase/$id/$depth/isonclust/ --k 13 --w 25 --q 6.0 --t 2 #&> /dev/null            
-python $isonclust_dir/isONclust write_fastq --clusters $outbase/$id/$depth/isonclust/final_clusters.csv --fastq $outbase/$id/$depth/reads.fq --outfolder $outbase/$id/$depth/isonclust/fastq --N 1  #&> /dev/null            
+# rm -rf $outbase/$depth/isonclust/sorted.fastq
+# echo python $isonclust_dir/isONclust --fastq $outbase/$depth/reads.fq --outfolder $outbase/$depth/isonclust/ --k 13 --w 25 --q 6.0 --t $cores
+# python $isonclust_dir/isONclust --fastq $outbase/$depth/reads.fq --outfolder $outbase/$depth/isonclust/ --k 13 --w 25 --q 6.0 --t 2 #&> /dev/null            
+# python $isonclust_dir/isONclust write_fastq --clusters $outbase/$depth/isonclust/final_clusters.tsv --fastq $outbase/$depth/reads.fq --outfolder $outbase/$depth/isonclust/fastq --N 10  #&> /dev/null            
 ############################
 
-# Correct reads with isONcorrect
-python $isoncorrect_dir/run_isoncorrect --keep_old --t $cores --fastq_folder $outbase/$id/$depth/isonclust/fastq  --outfolder $outbase/$id/$depth/isoncorrect/ --k 7 --w 10 --xmax 80 --exact_instance_limit 100  # &> /dev/null            
-###############################
+# # Correct reads with isONcorrect
+# python $isoncorrect_dir/run_isoncorrect --keep_old --t $cores --fastq_folder $outbase/$depth/isonclust/fastq  --outfolder $outbase/$depth/isoncorrect/ --k 7 --w 10 --xmax 80 --exact_instance_limit 100  # &> /dev/null            
+# ###############################
 
 
-# Evaluate individually corrected clusters    
-> $corrected_reads_fastq
+# Evaluate indiviually corrected clusters    
+# > $corrected_reads_fastq
 # FILES="$outbase/$id/$depth/isoncorrect"/*/corrected_reads.fastq
 # find $outbase/1/$depth/isoncorrect/*
-for folder in $(find $outbase/1/$depth/isoncorrect/*  -type d); #c_id in $(seq 0 1 1000) $"${FILES[@]}"
+for folder in $(find $outbase/$depth/isoncorrect/*  -type d); #c_id in $(seq 0 1 1000) $"${FILES[@]}"
     do 
         # echo $folder
         cl_id=$(basename $folder)
         echo $cl_id
-        if (( $cl_id > 50 ));
-            then
-                break
-        fi
+        # if (( $cl_id > 50 ));
+        #     then
+        #         break
+        # fi
 
         # if [ *$cl_id*  == "evaluation_original isoncorrect"  ]; then
         #   echo "Strings are equal" $cl_id
@@ -89,7 +89,7 @@ for folder in $(find $outbase/1/$depth/isoncorrect/*  -type d); #c_id in $(seq 0
             python $eval_dir/evaluate_simulated_reads.py --no_ref_sim $folder/corrected_reads.fastq $database_filtered $folder/evaluation  #> /dev/null
             awk -F "," -v awk_id=$cl_id -v awk_depth=$depth '{if (NR!=1) {print awk_id",isoncorrect,"awk_depth",0,"$0}}'  $folder/evaluation/results.csv >> $results_file
             
-            python $eval_dir/evaluate_simulated_reads.py --no_ref_sim $outbase/1/$depth/isonclust/fastq/$cl_id.fastq $database_filtered $folder/evaluation_reads  #> /dev/null
+            python $eval_dir/evaluate_simulated_reads.py --no_ref_sim $outbase/$depth/isonclust/fastq/$cl_id.fastq $database_filtered $folder/evaluation_reads  #> /dev/null
             awk -F "," -v awk_id=$cl_id -v awk_depth=$depth '{if (NR!=1) {print awk_id",original,"awk_depth",0,"$0}}'  $folder/evaluation_reads/results.csv >> $results_file
 
             # echo "$outbase/$id/$depth/isoncorrect"/$c_id/corrected_reads.fastq
