cee6977db6a792f01e7abb08a26740230c700e68
Kristoffer Sahlin <kxs624@psu.edu>
2019-08-07 17:06:31 +0300

    added features to real read evaluator

--- scripts/evaluate_real_reads.py
+++ scripts/evaluate_real_reads.py
@@ -4,7 +4,9 @@ import os,sys
 import argparse
 import re
 import errno
+import itertools
 
+import parasail
 import pysam
 
 '''
@@ -144,6 +146,7 @@ def cigar_to_seq_mm2_local(read, full_r_seq, full_q_seq):
     cigar_tuples = read.cigartuples
     r_end, m_end, q_end = "", "", ""
     ins, del_, subs, matches = 0, 0, 0, 0
+    # print(cigar_tuples)
     for (op_type, op_len) in cigar_tuples:
         # print((op_type, op_len))
         # op_len = int(op_len)
@@ -244,51 +247,194 @@ def get_summary_stats(reads, quantile):
     return tot_ins, tot_del, tot_subs, tot_match, sum_aln_bases
 
 
+def print_quantile_values(alignments_dict):
+
+    alignments_sorted = sorted(alignments_dict.items(), key = lambda x: sum(x[1][0:3])/float(sum(x[1])) )
+    print(alignments_sorted[-5:])
+
+    sorted_error_rates = [ sum(tup[0:3])/float(sum(tup)) for acc, tup in alignments_sorted]
+
+    insertions = [ tup[0]/float(sum(tup)) for acc, tup in alignments_sorted]
+    deletions = [ tup[1]/float(sum(tup)) for acc, tup in alignments_sorted]
+    substitutions = [ tup[2]/float(sum(tup)) for acc, tup in alignments_sorted]
+
+    n = len(sorted_error_rates)
+    quantiles = [0, 1.0/20, 1.0/10, 1.0/4, 1.0/2, 3.0/4, 9.0/10, 19.0/20, 1 ]
+    quantile_errors = []
+    quantile_insertions = []
+    quantile_deletions = []
+    quantile_substitutions = []
+
+    for q in quantiles:
+        if q == 1:
+            quantile_errors.append(sorted_error_rates[int(q*n)-1])
+            quantile_insertions.append(insertions[int(q*n)-1])
+            quantile_deletions.append(deletions[int(q*n)-1])
+            quantile_substitutions.append(substitutions[int(q*n)-1])
+        else:
+            quantile_errors.append(sorted_error_rates[int(q*n)])
+            quantile_insertions.append(insertions[int(q*n)])
+            quantile_deletions.append(deletions[int(q*n)])
+            quantile_substitutions.append(substitutions[int(q*n)])
+
+    # quantile_errors = [sorted_error_rates[0], sorted_error_rates[int(n/20)], sorted_error_rates[int(n/10)], 
+    #             sorted_error_rates[int(n/4)], sorted_error_rates[int(n/2)], 
+    #             sorted_error_rates[int(3*n/4)], sorted_error_rates[int(9*n/10)], sorted_error_rates[int(19*n/20)], 
+    #             sorted_error_rates[-1]]
+
+    return quantile_errors, quantile_insertions, quantile_deletions, quantile_substitutions
+
+
+def cigar_to_seq(cigar, query, ref):
+    cigar_tuples = []
+    result = re.split(r'[=DXSMI]+', cigar)
+    i = 0
+    for length in result[:-1]:
+        i += len(length)
+        type_ = cigar[i]
+        i += 1
+        cigar_tuples.append((int(length), type_ ))
+
+    r_index = 0
+    q_index = 0
+    q_aln = []
+    r_aln = []
+    for length_ , type_ in cigar_tuples:
+        if type_ == "=" or type_ == "X":
+            q_aln.append(query[q_index : q_index + length_])
+            r_aln.append(ref[r_index : r_index + length_])
+
+            r_index += length_
+            q_index += length_
+        
+        elif  type_ == "I":
+            # insertion w.r.t. reference
+            r_aln.append('-' * length_)
+            q_aln.append(query[q_index: q_index + length_])
+            #  only query index change
+            q_index += length_
+
+        elif type_ == 'D':
+            # deletion w.r.t. reference
+            r_aln.append(ref[r_index: r_index + length_])
+            q_aln.append('-' * length_)
+            #  only ref index change
+            r_index += length_
+        
+        else:
+            print("error")
+            print(cigar)
+            sys.exit()
+
+    return  "".join([s for s in q_aln]), "".join([s for s in r_aln])
+
+
+def parasail_alignment(read, reference, x_acc = "", y_acc = "", match_score = 2, mismatch_penalty = -2, opening_penalty = 2, gap_ext = 1, ends_discrepancy_threshold = 0):
+    user_matrix = parasail.matrix_create("ACGT", match_score, mismatch_penalty)
+    result = parasail.sg_trace_scan_16(read, reference, opening_penalty, gap_ext, user_matrix)
+    if result.saturated:
+        print("SATURATED!")
+        result = parasail.sg_trace_scan_32(read, reference, opening_penalty, gap_ext, user_matrix)
+    if sys.version_info[0] < 3:
+        cigar_string = str(result.cigar.decode).decode('utf-8')
+    else:
+        cigar_string = str(result.cigar.decode, 'utf-8')
+    
+    read_alignment, ref_alignment = cigar_to_seq(cigar_string, read, reference)
+    return read_alignment, ref_alignment
+
+
 def main(args):
     reads = { acc : seq for i, (acc, (seq, qual)) in enumerate(readfq(open(args.reads, 'r')))}
     corr_reads = { acc : seq for i, (acc, (seq, qual)) in enumerate(readfq(open(args.corr_reads, 'r')))}
+
+    if args.align:
+        for acc in reads:
+            if acc not in corr_reads:
+                print(acc, "Not in corrected reads")
+            else:
+                orig_seq = reads[acc]
+                corr_seq = corr_reads[acc]
+                read_alignment, ref_alignment = parasail_alignment(orig_seq, corr_seq)
+                orig_dlen = [d_len for ch, d_len in  [(ch, len(list(_))) for ch, _ in itertools.groupby(read_alignment)] if ch == "-" ]
+                corr_dlen = [d_len for ch, d_len in  [(ch, len(list(_))) for ch, _ in itertools.groupby(ref_alignment)] if ch == "-" ] 
+                if corr_dlen:
+                    max_orig_dlen = max( orig_dlen )
+                else:
+                    max_orig_dlen = 0
+                if corr_dlen:
+                    max_corr_dlen = max( corr_dlen )
+                else:
+                    max_corr_dlen = 0
+
+                if max_corr_dlen > 10 or max_orig_dlen > 10:
+                    print("orig", acc, read_alignment)
+                    print("corr", acc, ref_alignment)
+                    print()
+
     refs = { acc : seq for i, (acc, (seq, _)) in enumerate(readfq(open(args.refs, 'r')))}
     # print(refs)
     orig = get_aln_stats_per_read(args.orig_sam, reads, refs)
     corr = get_aln_stats_per_read(args.corr_sam, corr_reads, refs)
+    print(len(orig),len(corr))
+    quantile_tot_orig, quantile_insertions_orig, quantile_deletions_orig, quantile_substitutions_orig = print_quantile_values(orig)
+    quantile_tot_corr, quantile_insertions_corr, quantile_deletions_corr, quantile_substitutions_corr = print_quantile_values(corr)
+
+    # alignments_stats = get_summary_stats(alignments_dict, 1.0)
+    # print("Original reads (total): ins:{0}, del:{1}, subs:{2}, match:{3}".format(*alignments_stats[:-1]), "tot aligned region (ins+del+subs+match):", alignments_stats[-1] )
+    # print("Original reads percent:{0}, del:{1}, subs:{2}, match:{3}".format(*[round(100*float(s)/alignments_stats[-1] , 1) for s in alignments_stats[:-1]]))
+    # print( "Num_aligned_reads", "Aligned bases", "tot_errors", "avg_error_rate", "median_read_error_rate", "upper_25_quant", "lower_25_quant", "min", "max")
+
 
     orig_stats = get_summary_stats(orig, 1.0)
-    # sum_aln_orig_bases = sum(orig_stats) #orig_stats[0] + orig_stats[2] + orig_stats[3] 
-    print("Original reads (total): ins:{0}, del:{1}, subs:{2}, match:{3}".format(*orig_stats[:-1]), "tot aligned region (ins+del+subs+match):", orig_stats[-1] )
-    print("Original reads percent:{0}, del:{1}, subs:{2}, match:{3}".format(*[round(100*float(s)/orig_stats[-1] , 1) for s in orig_stats[:-1]]))
+    # print("Original reads (total): ins:{0}, del:{1}, subs:{2}, match:{3}".format(*orig_stats[:-1]), "tot aligned region (ins+del+subs+match):", orig_stats[-1] )
+    # print("Original reads percent:{0}, del:{1}, subs:{2}, match:{3}".format(*[round(100*float(s)/orig_stats[-1] , 1) for s in orig_stats[:-1]]))
 
     corr_stats = get_summary_stats(corr, 1.0)
-    print("Corrected reads (total): ins:{0}, del:{1}, subs:{2}, match:{3}".format(*corr_stats[:-1]), "tot aligned region (ins+del+subs+match):", corr_stats[-1])
-    print("Corrected reads percent:{0}, del:{1}, subs:{2}, match:{3}".format(*[round(100*float(s)/corr_stats[-1] , 1) for s in corr_stats[:-1]]))
+    # print("Corrected reads (total): ins:{0}, del:{1}, subs:{2}, match:{3}".format(*corr_stats[:-1]), "tot aligned region (ins+del+subs+match):", corr_stats[-1])
+    # print("Corrected reads percent:{0}, del:{1}, subs:{2}, match:{3}".format(*[round(100*float(s)/corr_stats[-1] , 1) for s in corr_stats[:-1]]))
 
 
-    print( "Num_aligned_reads", "Aligned bases", "tot_errors", "avg_error_rate", "median_read_error_rate", "upper_25_quant", "lower_25_quant", "min", "max")
-    orig_sorted = sorted(orig.items(), key = lambda x: sum(x[1][0:3])/float(sum(x[1])) )
-    print(orig_sorted[-5:])
-    orig_sorted_error_rates = [ sum(tup[0:3])/float(sum(tup)) for acc, tup in orig_sorted]
-    orig_vals = [orig_sorted_error_rates[0], orig_sorted_error_rates[int(len(orig_sorted_error_rates)/20)], orig_sorted_error_rates[int(len(orig_sorted_error_rates)/10)], 
-                orig_sorted_error_rates[int(len(orig_sorted_error_rates)/4)], orig_sorted_error_rates[int(len(orig_sorted_error_rates)/2)], 
-                orig_sorted_error_rates[int(3*len(orig_sorted_error_rates)/4)], orig_sorted_error_rates[int(9*len(orig_sorted_error_rates)/10)], orig_sorted_error_rates[int(19*len(orig_sorted_error_rates)/20)], 
-                orig_sorted_error_rates[-1]]
+    # print( "Num_aligned_reads", "Aligned bases", "tot_errors", "avg_error_rate", "median_read_error_rate", "upper_25_quant", "lower_25_quant", "min", "max")
+    # orig_sorted = sorted(orig.items(), key = lambda x: sum(x[1][0:3])/float(sum(x[1])) )
+    # print(orig_sorted[-5:])
+
+    # orig_sorted_error_rates = [ sum(tup[0:3])/float(sum(tup)) for acc, tup in orig_sorted]
+    # orig_vals = [orig_sorted_error_rates[0], orig_sorted_error_rates[int(len(orig_sorted_error_rates)/20)], orig_sorted_error_rates[int(len(orig_sorted_error_rates)/10)], 
+    #             orig_sorted_error_rates[int(len(orig_sorted_error_rates)/4)], orig_sorted_error_rates[int(len(orig_sorted_error_rates)/2)], 
+    #             orig_sorted_error_rates[int(3*len(orig_sorted_error_rates)/4)], orig_sorted_error_rates[int(9*len(orig_sorted_error_rates)/10)], orig_sorted_error_rates[int(19*len(orig_sorted_error_rates)/20)], 
+    #             orig_sorted_error_rates[-1]]
 
 
-    corr_sorted = sorted(corr.items(), key = lambda x: sum(x[1][0:3])/float(sum(x[1])) )
-    print(corr_sorted[-5:])
-    corr_sorted_error_rates = [ sum(tup[0:3])/float(sum(tup)) for acc, tup in corr_sorted]
-    corr_vals = [corr_sorted_error_rates[0],corr_sorted_error_rates[int(len(corr_sorted_error_rates)/20)], corr_sorted_error_rates[int(len(corr_sorted_error_rates)/10)],
-                 corr_sorted_error_rates[int(len(corr_sorted_error_rates)/4)], corr_sorted_error_rates[int(len(corr_sorted_error_rates)/2)],
-                  corr_sorted_error_rates[int(3*len(corr_sorted_error_rates)/4)], corr_sorted_error_rates[int(9*len(corr_sorted_error_rates)/10)], corr_sorted_error_rates[int(19*len(corr_sorted_error_rates)/20)],
-                  corr_sorted_error_rates[-1]]
+    # corr_sorted = sorted(corr.items(), key = lambda x: sum(x[1][0:3])/float(sum(x[1])) )
+    # print(corr_sorted[-5:])
+    # corr_sorted_error_rates = [ sum(tup[0:3])/float(sum(tup)) for acc, tup in corr_sorted]
+    # corr_vals = [corr_sorted_error_rates[0],corr_sorted_error_rates[int(len(corr_sorted_error_rates)/20)], corr_sorted_error_rates[int(len(corr_sorted_error_rates)/10)],
+    #              corr_sorted_error_rates[int(len(corr_sorted_error_rates)/4)], corr_sorted_error_rates[int(len(corr_sorted_error_rates)/2)],
+    #               corr_sorted_error_rates[int(3*len(corr_sorted_error_rates)/4)], corr_sorted_error_rates[int(9*len(corr_sorted_error_rates)/10)], corr_sorted_error_rates[int(19*len(corr_sorted_error_rates)/20)],
+    #               corr_sorted_error_rates[-1]]
     
     print("Distribution of error rates (Percent)")
     print("Reads, Best, top 5%, top 10%, top 25%, Median, top 75%, top 90%, top 95%, Worst")
-    print("Original,{0},{1},{2},{3},{4},{5},{6},{7},{8}".format( *[round(100*round(x,3), 2) for x in orig_vals ] ))
-    print("Corrected,{0},{1},{2},{3},{4},{5},{6},{7},{8}".format( *[round(100*round(x,3), 2) for x in corr_vals ] ))
+    print("Original,{0},{1},{2},{3},{4},{5},{6},{7},{8}".format( *[round(100*round(x,3), 2) for x in quantile_tot_orig ] ))
+    print("Corrected,{0},{1},{2},{3},{4},{5},{6},{7},{8}".format( *[round(100*round(x,3), 2) for x in quantile_tot_corr ] ))
 
     outfile = open(os.path.join(args.outfolder, "results.csv"), "w")
-    outfile.write("Original,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n".format( *[round(100*round(x,3), 2) for x in orig_vals ], orig_stats[-1]))
-    outfile.write("Corrected,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}".format( *[round(100*round(x,3), 2) for x in corr_vals ], corr_stats[-1]))
+    outfile.write("Original,tot,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n".format( *[round(100*round(x,3), 2) for x in quantile_tot_orig ], orig_stats[-1]))
+    outfile.write("Corrected,tot,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n".format( *[round(100*round(x,3), 2) for x in quantile_tot_corr ], corr_stats[-1]))
+    
+    outfile.write("Original,ins,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n".format( *[round(100*round(x,3), 2) for x in quantile_insertions_orig ], orig_stats[-1]))
+    outfile.write("Corrected,ins,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n".format( *[round(100*round(x,3), 2) for x in quantile_insertions_corr ], corr_stats[-1]))
+
+    outfile.write("Original,del,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n".format( *[round(100*round(x,3), 2) for x in quantile_deletions_orig ], orig_stats[-1]))
+    outfile.write("Corrected,del,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n".format( *[round(100*round(x,3), 2) for x in quantile_deletions_corr ], corr_stats[-1]))
+
+    outfile.write("Original,subs,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n".format( *[round(100*round(x,3), 2) for x in quantile_substitutions_orig ], orig_stats[-1]))
+    outfile.write("Corrected,subs,{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}".format( *[round(100*round(x,3), 2) for x in quantile_substitutions_corr ], corr_stats[-1]))
+
     outfile.close()
+
+    print(len(orig),len(corr))
     # print(orig_sorted)
     # print(",".join([s for s in orig_stats]))
     # print(",".join([s for s in corr_stats]))
@@ -303,6 +449,7 @@ if __name__ == '__main__':
     parser.add_argument('corr_reads', type=str, help='Path to the corrected read file')
     parser.add_argument('refs', type=str, help='Path to the refs file')
     parser.add_argument('outfolder', type=str, help='Output path of results')
+    parser.add_argument('--align', action= "store_true", help='Include pairwise alignment of original and corrected read.')
 
     args = parser.parse_args()
 
