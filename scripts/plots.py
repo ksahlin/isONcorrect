a3ea191d09f1fba8e3d0bf0a04036432b5d45485
Sahlin <sahlkris@lm9-523-002.pc.helsinki.fi>
2019-09-10 13:20:08 +0300

    +

--- scripts/plots.py
+++ scripts/plots.py
@@ -42,12 +42,62 @@ def total_error_rate(input_csv, outfolder):
     plt.savefig(os.path.join(outfolder, "total_error_rate.pdf"))
     plt.close()
 
+def splice_site_classification(input_csv, outfolder):
+
+    indata = pd.read_csv(input_csv)
+    # print(len(df))
+    # indata = df.loc[df['q_acc'] == df['r_acc']]
+    # print(len(indata))
+
+    g = sns.catplot(x="read_type", y="error_rate", #col="Depth",
+                data=indata,  #hue="read_type", hue_order= ["corrected", "original"],
+                kind="violin", aspect=1)
+
+    g.set(ylim=(0,15))
+    g.set_ylabels("Error rate (%)")
+    g.set_xlabels("Method")
+
+    # ax = sns.boxplot(x="p", y=y, hue = "type", data=indata)
+    # ax.set_ylim(0,15)
+    # ax.set_ylabel("Error rate %")
+
+    plt.savefig(os.path.join(outfolder, "total_error_rate.eps"))
+    plt.savefig(os.path.join(outfolder, "total_error_rate.pdf"))
+    plt.close()
+
+
+
+def total_error_rate2(input_csv, outfolder):
+
+    indata = pd.read_csv(input_csv)
+    # print(len(df))
+    # indata = df.loc[df['q_acc'] == df['r_acc']]
+    # print(len(indata))
+    data =indata[indata.read_type == 'corrected']
+    sns.distplot(data['error_rate'], kde=True, label='Corrected', bins=200, hist_kws=dict(alpha=0.5))
+    data = indata[indata.read_type == 'original']
+    sns.distplot(data['error_rate'], kde=True, label='Original', bins=200, hist_kws=dict(alpha=0.5))
+
+
+    plt.xticks(np.arange(0, 20, step=1))
+    plt.xlim(0,20)
+    plt.xlabel("Difference to HG38 (%)")
+    plt.ylabel("Frequency")
+
+    # ax = sns.boxplot(x="p", y=y, hue = "type", data=indata)
+    # ax.set_ylim(0,15)
+    # ax.set_ylabel("Error rate %")
+
+    plt.savefig(os.path.join(outfolder, "total_error_rate2.eps"))
+    plt.savefig(os.path.join(outfolder, "total_error_rate2.pdf"))
+    plt.close()
 
 def main(args):
     
     sns.set(style="whitegrid")
 
-    total_error_rate(args.input_csv, args.outfolder)
+    # total_error_rate(args.input_csv, args.outfolder)
+    total_error_rate2(args.input_csv, args.outfolder)
 
 
 if __name__ == '__main__':
