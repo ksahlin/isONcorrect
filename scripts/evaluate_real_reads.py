9298c0a391b0f0c3f26bcf6cbe50cbe5cc6cfbb6
Kristoffer Sahlin <kxs624@psu.edu>
2019-10-19 17:45:52 +0300

    +

--- scripts/evaluate_real_reads.py
+++ scripts/evaluate_real_reads.py
@@ -17,7 +17,7 @@ import gffutils
 '''
     Below code taken from https://github.com/lh3/readfq/blob/master/readfq.py
 '''
-def readfq(fp): # this is a generator function
+def readfq_split(fp): # this is a generator function
     last = None # this is a buffer keeping the last unprocessed line
     while True: # mimic closure; is it a bad idea?
         if not last: # the first record or a record following a fastq
@@ -47,6 +47,38 @@ def readfq(fp): # this is a generator function
             if last: # reach EOF before reading enough quality
                 yield name, (seq, None) # yield a fasta record instead
                 break
+
+def readfq(fp): # this is a generator function
+    last = None # this is a buffer keeping the last unprocessed line
+    while True: # mimic closure; is it a bad idea?
+        if not last: # the first record or a record following a fastq
+            for l in fp: # search for the start of the next record
+                if l[0] in '>@': # fasta/q header line
+                    last = l[:-1] # save this line
+                    break
+        if not last: break
+        name, seqs, last = last[1:].split()[0], [], None
+        for l in fp: # read the sequence
+            if l[0] in '@+>':
+                last = l[:-1]
+                break
+            seqs.append(l[:-1])
+        if not last or last[0] != '+': # this is a fasta record
+            yield name, (''.join(seqs), None) # yield a fasta record
+            if not last: break
+        else: # this is a fastq record
+            seq, leng, seqs = ''.join(seqs), 0, []
+            for l in fp: # read the quality
+                seqs.append(l[:-1])
+                leng += len(l) - 1
+                if leng >= len(seq): # have read enough quality
+                    last = None
+                    yield name, (seq, ''.join(seqs)); # yield a fastq record
+                    break
+            if last: # reach EOF before reading enough quality
+                yield name, (seq, None) # yield a fasta record instead
+                break
+
 
 
 
@@ -927,8 +959,8 @@ def main(args):
         pickle_dump(annotated_splice_coordinates_pairs, os.path.join( args.outfolder, 'annotated_splice_coordinates_pairs.pickle') )
         pickle_dump(minimum_annotated_intron, os.path.join( args.outfolder, 'minimum_annotated_intron.pickle') )
 
-    reads = { acc.split()[0] : seq for i, (acc, (seq, qual)) in enumerate(readfq(open(args.reads, 'r')))}
-    corr_reads = { acc.split()[0] : seq for i, (acc, (seq, qual)) in enumerate(readfq(open(args.corr_reads, 'r')))}
+    reads = { acc.split()[0] : seq for i, (acc, (seq, qual)) in enumerate(readfq_split(open(args.reads, 'r')))}
+    corr_reads = { acc.split()[0] : seq for i, (acc, (seq, qual)) in enumerate(readfq_split(open(args.corr_reads, 'r')))}
     orig_primary_locations = decide_primary_locations(args.orig_sam, args)
     corr_primary_locations = decide_primary_locations(args.corr_sam, args)
 
