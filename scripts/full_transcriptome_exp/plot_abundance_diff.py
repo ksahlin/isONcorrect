1802424e63b4ab6360ac96e7666d8f761e6d899f
Kristoffer Sahlin <kxs624@psu.edu>
2019-11-23 14:18:37 +0200

    upd analysis of overcorrection

--- scripts/full_transcriptome_exp/plot_abundance_diff.py
+++ scripts/full_transcriptome_exp/plot_abundance_diff.py
@@ -22,8 +22,53 @@ data=sys.argv[1]
 # seq=sys.argv[2]
 
 # f = open(data, "r")
-df = pd.read_csv(data) #, dtype={'id': str, "cov_true": int, "cov_aln": int, "type": str})
-indata = df.loc[df['seq'] == 'transcript']
+indata = pd.read_csv(data) #, dtype={'id': str, "cov_true": int, "cov_aln": int, "type": str})
+list_of_list = indata.values.tolist()
+# print(list_of_list[-1])
+read_annot = {}
+orig_good = 0
+corr_good = 0
+both_good = 0
+both_bad = 0
+for row in list_of_list:
+    acc, annot, ab, is_corr, read_type = row
+    if acc not in read_annot:
+        read_annot[acc] = [None,None, ab]
+
+    if read_type == 'corrected':
+        read_annot[acc][1] =  is_corr
+    else:
+        read_annot[acc][0] =  is_corr
+
+wrong_in_both = []
+wrong_in_orig = []
+wrong_in_corr = []
+
+for acc in read_annot:
+    orig, corr, ab = read_annot[acc]
+    if orig == 1 and corr == 1:
+        both_good += 1
+    elif  orig == 1 and corr == 0:
+        orig_good += 1
+        wrong_in_corr.append(acc)
+    elif  orig == 0 and corr == 1:
+        corr_good += 1
+        wrong_in_orig.append(acc)
+    else:
+        both_bad += 1
+        wrong_in_both.append(acc)
+
+print(wrong_in_both)
+print(wrong_in_orig)
+# print(sum(wrong_in_both)/float(len(wrong_in_both)))
+# print(sum(wrong_in_orig)/float(len(wrong_in_orig)))
+# print(sum(wrong_in_corr)/float(len(wrong_in_corr)))
+print(wrong_in_corr)
+print(both_good, orig_good, corr_good, both_bad)
+
+
+
+# indata = df.loc[df['seq'] == 'transcript']
 # print(indata.info())
 # indata[["id"]] = indata.apply(pd.to_str)
 # indata[["cov_true", "cov_aln"]] = indata.apply(pd.to_numeric)
@@ -38,11 +83,11 @@ indata = df.loc[df['seq'] == 'transcript']
 # g = sns.lmplot(x="cov_true", y="cov_aln",  hue="type", scatter = False, x_ci= 'sd', #x_bins=50, #x_jitter = 0.3, #fit_reg= True, x_jitter = 0.2, y_jitter = 0.2, #col="Depth", 
 #                data=indata, scatter_kws={'alpha':0.3}) #, col_wrap=2, height=3)
 
-ax = sns.lineplot(x="cov_true", y="cov_aln",  hue="type", 
+ax = sns.lineplot(x="transcript_abundance", y="is_tp",  hue="read_type", 
                   ci = 'sd', data=indata)
 # ax.set_xscale('log')
 # ax.set_yscale('log')
-ax.set_ylabel("Aligned transcript abundance")
+ax.set_ylabel("Fraction correctly aligned back to true transcript")
 ax.set_xlabel("True transcript abundance")
 # g.set(xscale="log", yscale="log")
 # g.set(ylim=(0,100))
