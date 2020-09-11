f9d231836acba92894d7224c639dd1bc788ef32a
Kristoffer Sahlin <kxs624@psu.edu>
2020-09-11 14:42:16 +0200

    +

---
+++ scripts/sum_exon_lengths.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+import os,sys
+import argparse
+import re
+# import ssw
+import numpy as np
+from Bio import pairwise2
+from Bio.SubsMat import MatrixInfo as matlist
+import pandas as pd
+import string
+import fractions
+from collections import Counter
+import parasail
+from collections import defaultdict
+
+
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+'''
+    Below code taken from https://github.com/lh3/readfq/blob/master/readfq.py
+'''
+def readfq(fp): # this is a generator function
+    last = None # this is a buffer keeping the last unprocessed line
+    while True: # mimic closure; is it a bad idea?
+        if not last: # the first record or a record following a fastq
+            for l in fp: # search for the start of the next record
+                if l[0] in '>@': # fasta/q header line
+                    last = l[:-1] # save this line
+                    break
+        if not last: break
+        name, seqs, last = last[1:].replace(" ", "_"), [], None
+        for l in fp: # read the sequence
+            if l[0] in '@+>':
+                last = l[:-1]
+                break
+            seqs.append(l[:-1])
+        if not last or last[0] != '+': # this is a fasta record
+            yield name, (''.join(seqs), None) # yield a fasta record
+            if not last: break
+        else: # this is a fastq record
+            seq, leng, seqs = ''.join(seqs), 0, []
+            for l in fp: # read the quality
+                seqs.append(l[:-1])
+                leng += len(l) - 1
+                if leng >= len(seq): # have read enough quality
+                    last = None
+                    yield name, (seq, ''.join(seqs)); # yield a fastq record
+                    break
+            if last: # reach EOF before reading enough quality
+                yield name, (seq, None) # yield a fasta record instead
+                break
+
+
+
+fa_file = sys.argv[1]
+
+lengths = [len(seq) for acc, (seq,_) in readfq(open(fa_file, 'r'))]
+
+print(lengths)
+print("sum seq lengths:", sum(lengths))
