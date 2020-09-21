d3ecb0f8b17458e5a30be30b446a9373350bcb35
Kristoffer Sahlin <kxs624@psu.edu>
2020-09-21 21:57:52 +0200

    initail commit of sirv exon pertubation experiment

---
+++ scripts/exon_perturbation_sirv/plot_mutation_data.py
@@ -0,0 +1,47 @@
+import sys
+import argparse
+import os
+import random
+try:
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+except (ImportError, RuntimeError):
+    print("COULD not import matplotlib")
+# import matplotlib.pyplot as plt
+# import matplotlib
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
+sns.set(style="whitegrid")
+
+data=sys.argv[1]
+df = pd.read_csv(data)
+print(len(df))
+df['q_acc'] = df['q_acc'].apply(lambda x: x.split("_")[0])
+indata = df.loc[df['q_acc'] == df['r_acc']]
+print(len(indata))
+
+y=sys.argv[3]
+g = sns.catplot(x="p", y=y, col="Depth", col_wrap=3,
+            data=indata, hue="type", hue_order= ["exact", "approx", "original"],
+            kind="violin", aspect=1)
+
+
+g.set(ylim=(0,15))
+# g.set(yscale="log", yticks=[1e-1,1e0,1e1,1e2])
+# g.set_yticks([1e-1,1e0,1e1,1e2])
+# g.set_yticklabels(("","0.1%","1%","10%","100%"), visible = True)
+g.set_ylabels("Error rate")
+g.set_xlabels("Fraction mutation present")
+
+# ax = sns.boxplot(x="p", y=y, hue = "type", data=indata)
+# ax.set_ylim(0,15)
+# ax.set_ylabel("Error rate %")
+
+plt.savefig(sys.argv[2])
+plt.close()
+
+
