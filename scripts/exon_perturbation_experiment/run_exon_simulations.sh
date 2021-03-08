98ae0dc8a48756e86b2955b80f8aaf4027860fe5
Kristoffer Sahlin <krsahlin@gmail.com>
2021-03-08 10:19:13 +0100

    +

--- scripts/exon_perturbation_experiment/run_exon_simulations.sh
+++ scripts/exon_perturbation_experiment/run_exon_simulations.sh
@@ -36,16 +36,16 @@ echo -n  "id","type","Depth","p","q_acc","r_acc","total_errors","error_rate","ab
 
 RTN4IP1="ATGCTATTGGTCTTCGAATGCCGGAGTGGGAGCTGTTGAACGGTTCTAAGTAGGAGAGTGAAAGACCAGCATGGAGGAGAAATCCGGCCAGTTATTGAACAAACCTTTCCTTTTTCTAAAGTTCCAGAAGCCTTCCTGAAGGTGGAAAGAGGACACGCACGAGGAAAGACTGTAATTAATGTTGTTTAAATAAAAATGCAGTTTAGTG"
 # MAP3K4="CTTCCTGGAACGCAGAGTGTAATCCCTGCATTTGGCAAGAGGGCACATTGGGAGATTCAGCTTGCAAGAGTCCTGAATCTGATCTAGAAGACTTCTCCGATGAAACAAATACAGAGAATCTTTATGGTACCTCTCCCCCCAGCACACCTCGACAGATGAAACGCATGTCAACCAAACATCAGAGGAATAATGTGGGGAGGCCAGCCAGTCGGTCTAATTTGAAAG"
-for id in $(seq 1 1 10) #  $(seq 1 1 10)
+for id in $(seq 1 1 1) #  $(seq 1 1 10)
 do 
     # sim random mutation
-    python $experiment_dir/simulate_reads.py --sim_exon_removal --exon_length 5 --ref $RTN4IP1 --outfolder $outbase/$id/  > /dev/null
+    # python $experiment_dir/simulate_reads.py --sim_exon_removal --exon_length 10 --ref $RTN4IP1 --outfolder $outbase/$id/  > /dev/null
 
     for depth in 10 20 40 60 80 100
     do
         for p in 0.1 0.2 0.3 0.4 0.5
         do
-            python $experiment_dir/simulate_reads.py --isoforms $outbase/$id/isoforms.fa --outfolder $outbase/$id/$depth/$p --probs $p  --nr_reads $depth > /dev/null
+            # python $experiment_dir/simulate_reads.py --isoforms $outbase/$id/isoforms.fa --outfolder $outbase/$id/$depth/$p --probs $p  --nr_reads $depth > /dev/null
 
             # python $inbase/isONcorrect --fastq $outbase/$id/$depth/$p/reads.fq   --outfolder $outbase/$id/$depth/$p/isoncorrect/  &> /dev/null            
             # python $experiment_dir/evaluate_simulated_reads.py  $outbase/$id/$depth/$p/isoncorrect/corrected_reads.fastq  $outbase/$id/isoforms.fa  $outbase/$id/$depth/$p/isoncorrect/evaluation > /dev/null
@@ -53,7 +53,7 @@ do
             # echo -n  $id,approx,$depth,$p, >> $summary_file && head -n 1 $outbase/$id/$depth/$p/isoncorrect/evaluation/summary.csv >> $summary_file
             # awk -F "," -v awk_id=$id -v awk_depth=$depth -v awk_p=$p  '{if (NR!=1) {print awk_id",approx,"awk_depth","awk_p","$0}}'  $outbase/$id/$depth/$p/isoncorrect/evaluation/results.csv >> $results_file
 
-            python $inbase/isONcorrect --fastq $outbase/$id/$depth/$p/reads.fq   --outfolder $outbase/$id/$depth/$p/isoncorrect_exact/ &> /dev/null            
+            python $inbase/isONcorrect --fastq $outbase/$id/$depth/$p/reads.fq   --outfolder $outbase/$id/$depth/$p/isoncorrect_exact/ --w 17 --paired_minimizers &> /dev/null            
             python $experiment_dir/evaluate_simulated_reads.py  $outbase/$id/$depth/$p/isoncorrect_exact/corrected_reads.fastq  $outbase/$id/isoforms.fa  $outbase/$id/$depth/$p/isoncorrect_exact/evaluation  > /dev/null
             echo -n  $id,exact,$depth,$p,&& head -n 1 $outbase/$id/$depth/$p/isoncorrect_exact/evaluation/summary.csv 
             echo -n  $id,exact,$depth,$p, >> $summary_file && head -n 1 $outbase/$id/$depth/$p/isoncorrect_exact/evaluation/summary.csv >> $summary_file
